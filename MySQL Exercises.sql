4360b6ac1286e25a40991aac27cc18b93dbff77c
Claire <48715144+Claire-yyf@users.noreply.github.com>
2019-03-22 21:37:12 +1300

    Update MySQL Exercises.sql

--- MySQL Exercises.sql
+++ MySQL Exercises.sql
@@ -1,6 +1,7 @@
-MySQL Exercises
+# MySQL Exercises
 
 
+/*
 63. Table Constrain Ex
 Define an Employees table, with the following fields:
 id - number(automatically increments), mandatory, primary key
@@ -9,6 +10,7 @@ first_name - text, mandatory
 middle_name - text, not mandatory
 age - number mandatory
 current_status - text, mandatory, defaults to 'employed'
+*/
 
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT,
@@ -18,4 +20,13 @@ CREATE TABLE employees (
     age INTEGER NOT NULL,
     current_status VARCHAR(100) NOT NULL DEFAULT 'employed',
     PRIMARY KEY (id)
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    middle_name VARCHAR(255),
+    age INTEGER NOT NULL,
+    current_status VARCHAR(100) NOT NULL DEFAULT 'employed'
 );

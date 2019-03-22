500b45ead461e14b947e8175ae49bc70e7fcc9cd
Claire <48715144+Claire-yyf@users.noreply.github.com>
2019-03-22 21:56:26 +1300

    Add files via upload

---
+++ 0-Exercises.sql
@@ -0,0 +1,32 @@
+# 0-Exercises
+
+
+/*
+63. Table Constrain Ex
+Define an Employees table, with the following fields:
+id - number(automatically increments), mandatory, primary key
+last_name - text, mandatory
+first_name - text, mandatory
+middle_name - text, not mandatory
+age - number mandatory
+current_status - text, mandatory, defaults to 'employed'
+*/
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT,
+    last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    middle_name VARCHAR(255),
+    age INTEGER NOT NULL,
+    current_status VARCHAR(100) NOT NULL DEFAULT 'employed',
+    PRIMARY KEY (id) # assign primary key here
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, # assign primary key here
+    last_name VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    middle_name VARCHAR(255),
+    age INTEGER NOT NULL,
+    current_status VARCHAR(100) NOT NULL DEFAULT 'employed'
+);

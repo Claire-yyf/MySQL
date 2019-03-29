e6884a22bf266c240af8cf23ebc40af8fad2c048
Claire <48715144+Claire-yyf@users.noreply.github.com>
2019-03-29 16:42:56 +1300

    Update 0-Exercises.sql

--- 0-Exercises.sql
+++ 0-Exercises.sql
@@ -156,3 +156,31 @@ SELECT * FROM shirts; -- empty set
 DROP TABLE shirts; 
 SHOW TABLES; -- empty set
 DESC shirts; -- error because shirts table does not exist  
+ 
+
+/*
+118. String Function Exercises
+*/
+
+SELECT UPPER(REVERSE('Why does my cat look at me with such hatred?')); -- ?DERTAH HCUS HTIW EM TA KOOL TAC YM SEOD YHW 
+SELECT REPLACE(CONCAT('I', ' ', 'like', ' ', 'cats'), ' ', '-'); -- I-like-cats
+
+SELECT REPLACE(title, ' ', '->') AS title 
+FROM books; 
+
+SELECT author_lname AS forwards, REVERSE(author_lname) AS backwards 
+FROM books; 
+
+SELECT UPPER(CONCAT(author_fname, ' ', author_lname)) AS 'full name in caps' 
+FROM books;
+
+SELECT CONCAT(title, ' was released in ', released_year) AS blurb 
+FROM books;
+
+SELECT title, CHAR_LENGTH(title) AS 'character count' 
+FROM books;
+
+SELECT CONCAT(SUBSTRING(title, 1, 10), '...') AS 'short title',
+        CONCAT(author_lname, ',', author_fname) AS author,
+        CONCAT(stock_quantity, ' in stock') AS quantity
+FROM books;
